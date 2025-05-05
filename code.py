64308aaec32e2c65b047537662b1e18d3f1a5e17
joaopedrorb7 <joaopedroribarbosa@gmail.com>
2025-05-05 13:08:38 -0300

    Add files via upload

---
+++ code.py
@@ -0,0 +1,24 @@
+import time
+
+mensagem = [
+    "Feliz Dia das Mães!",
+    "❤️❤️❤️❤️❤️❤️❤️❤️❤️❤️❤️❤️",
+    "Mãe, obrigado por tudo que fez por mim.",
+    "A senhora é muito especial para mim, meu exemplo de vida.",
+    "Te Amo muito!",
+    "💖💖💖💖💖💖💖💖💖💖💖💖" 
+]
+
+for linha in mensagem:
+    print(linha)
+    time.sleep(1.5)
+
+print()
+print("    **     **")
+print("  ****** ******")
+print(" **************")
+print("  ************")
+print("   **********")
+print("     ******")
+print("      ****")
+print("       **")
